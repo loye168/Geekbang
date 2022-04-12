93682cf1fb97190e9985cc51e2ea860b3a3a5040
loye <ling16852@163.com>
2022-04-12 23:29:55 +0800

    update

--- Week_2/TwoNumbers.java
+++ Week_2/TwoNumbers.java
@@ -1,7 +1,7 @@
 //两数之和
 public class TwoNumbers{
 	public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map = new ConcurrentHashMap<>();
+         Map<Integer,Integer> map = new ConcurrentHashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int t1 = target - nums[i];
             if(map.containsKey(t1)){
